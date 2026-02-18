2d2abcd68ece5c4ceb367ea19afb32f28650d92a
ThirishnaviPushparajah <thirishnavi@smartzi.com>
2026-02-18 22:25:15 +0530

    Initial commit in thirish branch

---
+++ frontend/src/Components/Features/GeoLocation/Location.jsx
@@ -0,0 +1,8 @@
+export default function Location() {
+  return (
+    <div className="location">
+      <h2>Location</h2>
+      <p>We are located at 123 Main Street, Anytown, USA.</p>
+    </div>
+  );
+}
